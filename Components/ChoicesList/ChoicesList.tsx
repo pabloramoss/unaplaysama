747e5d5744fd9450e02f37661f7e42d24c7f136a
Pablo Ramos <pablo-362@hotmail.com>
2022-08-11 22:26:19 -0300

    add chakra and ChoicesList component

---
+++ Components/ChoicesList/ChoicesList.tsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+import {Textarea, Text, Button, Stack} from "@chakra-ui/react"
+
+interface ChoicesListProps {
+  setChoices: any;
+}
+
+const ChoicesList: React.FC<ChoicesListProps> = ({setChoices})=> {
+  const [text, setText] = useState("")
+
+  const onChangeText = (event:React.ChangeEvent<HTMLTextAreaElement>) => {
+    setText(event.target.value)
+    console.log(event.target.value)
+  }
+  
+
+  return(
+    <Stack maxW={400}>
+      <Text>Pega las opciones en la caja de texto debajo, una opcion por linea y luego haz click en "Agregar opciones"</Text>
+      <Textarea onChange={onChangeText} placeholder='Agrega una opcion por linea' height="300px" width="100%" overflowY="auto" />
+      <Button onClick={() => setChoices(text.split("\n"))} colorScheme='light' bg="gray.500" _hover={{bg: "gray.600"}}>Agregar opciones</Button>
+      {`<ChoicesList />`}
+    </Stack>
+  )
+}
+export default ChoicesList
