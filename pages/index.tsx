bcc24b4accea93d60bdaf6593f30dafd92b00bca
Pablo Ramos <pablo-362@hotmail.com>
2022-08-18 22:44:38 -0300

    add new wheel

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import Head from 'next/head'
 import { useState } from 'react'
 import ChoicesList from '../Components/ChoicesList'
 import Wheel from '../Components/Wheel/Wheel'
+import Wheel2 from '../Components/Wheel/Wheel2'
 
 const Home: NextPage = () => {
   const [choices, setChoices] = useState([])
@@ -22,6 +23,7 @@ const Home: NextPage = () => {
           <img style={{borderRadius: "9999px"}} height={500} width={500} src="https://via.placeholder.com/500" />
         </HStack>
         <Wheel />
+        <Wheel2 />
       </main>
 
       <footer>
